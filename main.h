1b66ffd0f9adf4af71295f340f81146c0cdf826a
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-11-23 23:39:31 +0100

    corrected errors

--- main.h
+++ main.h
@@ -60,10 +60,10 @@ int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* Function to print memory address */
 int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
+/* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
@@ -76,12 +76,16 @@ int print_reverse(va_list types, char buffer[], int flags, int width, int precis
 int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 /* width handler */
-int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int handle_write_char(char c, char buffer[],
+int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+int flags, int width, int precision, int size);
+int write_num(int ind, char bff[],
+int flags, int width, int precision, int length, char padd, char extra_c);
+int write_pointer(char buffer[],
+int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
 
-int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_unsgnd(int to is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
 int is_printable(char);
