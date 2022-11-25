6667c47376deca181ab0ebd38d9b9f02bdb5cbfa
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-11-25 04:52:09 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,66 +1,49 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdio.h>
 
 /**
- *_printf - Printf function
- *@format: format.
- *Return: Printed chars.
- **/
+ * _printf - The main printf function
+ * @format: string format
+ * Return: count of characters in @format
+ */
+
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	va_list args;
+	const char *str;
+	int (*func)(va_list, flag_t *, mod_t *);
+	flag_t f = {0, 0, 0};
+	mod_t m = {0, 0, 0};
+	int i, count = 0, num = 0;
 
-	if (format == NULL)
+	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (str = format; *str; str++)
 	{
-		if (format[i] != '%')
+		if (*str == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+			str++;
+			if (*str == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			for (; get_flags(*str, &f); str++)
+			{}
+			for (i = 0; *str >= 48 && *str <= 57; str++, i++)
+				num = (num * (i * 10)) + (*str - '0');
+			set_width(num, &m);
+			for (; get_modifier(*str, &m); str++)
+			{}
+			func = get_func(*str);
+			count += (func) ? func(args, &f, &m)
+				: _printf("%%%c", *str);
+		} else
+			count += _putchar(*str);
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- *print_buffer - Prints the contents of the buffer if it exist
- *@buffer: Array of chars
- *@buff_ind: Index at which to add next char, represents the length.
- **/
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	_putchar(-1), va_end(args);
+	return (count);
 }
